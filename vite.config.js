5543003fc8c387238afb93dbb7aa13887dc430f4
Ahmet DALDEMİR <ahmetdaldemirdeveloper@gmail.com>
2025-06-25 18:52:35 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  server: {
+    port: 5173,
+    host: true
+  }
 })
